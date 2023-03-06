fb7941abeededdec28c2b0e0bcc79d305d1e8e15
EdwardGold Edem Hogan <edge37@outlook.com>
2023-03-06 09:27:44 +0100

    ..

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const IPFS = require("ipfs");
-IPFS.create({
-  config: {
-    Addresses: {
-      Swarm: ["/ip4/127.0.0.1/tcp/10000/", "/ip4/127.0.0.1/tcp/10000/ws"],
-    },
-  },
+import { create } from 'ipfs'
+create({
+    config: {
+        Addresses: {
+            Swarm: ["/ip4/127.0.0.1/tcp/10000/", "/ip4/127.0.0.1/tcp/10000/ws"]
+        }
+    }
 });
---
+++ index.ts
@@ -0,0 +1,8 @@
+const { IPFS } = require("ipfs");
+IPFS.create({
+  config: {
+    Addresses: {
+      Swarm: ["/ip4/127.0.0.1/tcp/10000/", "/ip4/127.0.0.1/tcp/10000/ws"],
+    },
+  },
+});
