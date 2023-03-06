d02c002ded15e082c95a48d41b454187b786e8dc
EdwardGold Edem Hogan <edge37@outlook.com>
2023-03-06 17:37:45 +0100

    haha

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-import { create } from 'ipfs'
-create({
-    config: {
-        Addresses: {
-            Swarm: ["/ip4/0.0.0.0/tcp/10000/", "/ip4/0.0.0.0/tcp/10000/ws"]
-        }
-    }
+import http from "http";
+import { create } from "ipfs";
+
+const ipfs_port = 3737;
+const health_port = 10000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/health") {
+    res.writeHead(200);
+    res.end();
+  }
 });
 
+server.listen(health_port, () => {
+  console.log(`Server is running on port ${health_port}`);
+});
 
+create({
+  config: {
+    Addresses: {
+      Swarm: [
+        `/ip4/0.0.0.0/tcp/${ipfs_port}/`,
+        `/ip4/0.0.0.0/tcp/${ipfs_port}/ws`,
+      ],
+    },
+  },
+});
